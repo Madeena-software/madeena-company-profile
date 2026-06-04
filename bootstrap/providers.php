3cb566d7131ef27baff04eac37cd81fa4b3be827
faliqadlan <faliqadlan67@gmail.com>
2026-06-04 12:45:00 +0700

    feat: migrate backup infrastructure from WebDAV to S3/MinIO in CI/CD pipelines and remove WebDAV provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,9 @@
 <?php
-
+ 
 use App\Providers\AppServiceProvider;
 use App\Providers\Filament\AdminPanelProvider;
-use App\Providers\WebDavFilesystemServiceProvider;
-
+ 
 return [
     AppServiceProvider::class,
-    WebDavFilesystemServiceProvider::class,
     AdminPanelProvider::class,
 ];
