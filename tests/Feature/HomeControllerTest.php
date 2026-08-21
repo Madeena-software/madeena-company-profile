9f7424d2fc8c060b7582fb581b626048b1908624
root <root@adlan.localdomain>
2026-08-21 09:48:33 +0700

    fix(routes): align article routes with artikel naming

--- tests/Feature/HomeControllerTest.php
+++ tests/Feature/HomeControllerTest.php
@@ -17,10 +17,24 @@ class HomeControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_blog_index_loads_successfully()
+    public function test_artikel_index_loads_successfully()
     {
-        $response = $this->get(route('blog.index'));
+        $this->assertEquals('/artikel', route('artikel.index', [], false));
+
+        $user = \App\Models\User::factory()->create();
+        Post::create([
+            'title' => 'Test Artikel 1',
+            'slug' => 'test-artikel-1',
+            'content_json' => [],
+            'excerpt' => 'Test Excerpt',
+            'user_id' => $user->id,
+            'published_at' => now(),
+            'is_published' => true,
+        ]);
+
+        $response = $this->get(route('artikel.index'));
         $response->assertStatus(200);
+        $response->assertSee('Test Artikel 1');
     }
 
     public function test_product_show_loads_successfully()
@@ -36,7 +50,7 @@ class HomeControllerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    public function test_post_show_loads_successfully()
+    public function test_post_show_loads_successfully_when_published()
     {
         $user = \App\Models\User::factory()->create();
         $post = Post::create([
@@ -49,7 +63,28 @@ class HomeControllerTest extends TestCase
             'is_published' => true
         ]);
 
+        $this->assertEquals('/artikel/test-post', route('post.show', $post, false));
+
         $response = $this->get(route('post.show', $post));
         $response->assertStatus(200);
+        $response->assertSee(route('artikel.index'));
+        $response->assertSee('Kembali ke Artikel');
+    }
+
+    public function test_post_show_returns_404_when_unpublished()
+    {
+        $user = \App\Models\User::factory()->create();
+        $post = Post::create([
+            'title' => 'Unpublished Post',
+            'slug' => 'unpublished-post',
+            'content_json' => [],
+            'excerpt' => 'Draft Excerpt',
+            'user_id' => $user->id,
+            'published_at' => null,
+            'is_published' => false,
+        ]);
+
+        $response = $this->get(route('post.show', $post));
+        $response->assertStatus(404);
     }
 }
